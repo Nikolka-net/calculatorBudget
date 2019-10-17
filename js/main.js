2d443e5c0a7e71cc609e484f99409121aa523f98
Nikolay <nikolka-net@mail.ru>
2019-10-17 22:59:52 +0600

    Добавлены переменные, alert и console.log в main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let money = 10000,
+    income = 500,
+    addExpenses = 120,
+    deposit = 2000,
+    mission = 'become frontend developer',
+    period = '6 month';
+
+//alert(mission);
+//console.log('addExpenses : ', addExpenses);
+
+alert('Всем привет!');
+
+console.log('no errors');
