74416520e168c24f4689c78a182a260ae1f66f01
Nikolay <nikolka-net@mail.ru>
2019-10-22 19:38:36 +0600

    Добавлены money, deposit, addExpenses

--- js/main.js
+++ js/main.js
@@ -1,13 +1,62 @@
-let money = 10000,
-    income = 500,
-    addExpenses = 120,
-    deposit = 2000,
+'use strict';
+
+let income = 500,
     mission = 'become frontend developer',
-    period = '6 month';
+    period = '6 month',
+    money;
 
-//alert(mission);
-//console.log('addExpenses : ', addExpenses);
+function moneyDepositExpenses() {
 
-alert('Всем привет!');
 
-console.log('no errors');
+    function moneyPrompt() {
+
+        let n = prompt('Ваш месячный доход?', '25000');
+
+        if (!isNaN(parseFloat(n)) && isFinite(n) == true) {
+            money = Number(n);
+        } else {
+            alert('Введите число');
+            moneyPrompt();
+        }
+    }
+
+    moneyPrompt();
+
+    function addExpensesPrompt() {
+
+        let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+        addExpenses = addExpenses.split(', ');
+        console.log(addExpenses);//выводим в виде массива
+    }
+
+    addExpensesPrompt();
+
+
+
+    let deposit = prompt('Есть ли у вас депозит в банке?', 'Да');
+
+    if (deposit == 'Да') {
+        deposit = Boolean(1);
+    } else if (deposit == 'Нет') {
+        deposit = Boolean(0);
+    } else {
+        alert('Введите корректное значение');
+    }
+
+
+
+    console.log(typeof deposit);
+    console.log(typeof money);
+    console.log(typeof income);
+
+}
+
+moneyDepositExpenses();
+
+let addExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+let addExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+let spending1 = prompt('Во сколько это обойдётся?');
+let spending2 = prompt('Во сколько это обойдётся?');
+
+let proceeds = money - (addExpenses1 + addExpenses2 + spending1 + spending2);
+console.log(proceeds);
