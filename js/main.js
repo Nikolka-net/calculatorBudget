01a6cef3eef905573b1055f846c204a5116c9036
Nikolay <nikolka-net@mail.ru>
2019-10-24 20:06:54 +0600

    Добавил накопления за период periodAccumulate

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
 let mission = 100000,
-    period = 5,
-    money;
+  period = 5,
+  money;
 
 function getMoney() {
-    let n = prompt('Ваш месячный доход?', 30000);
-    if (!isNaN(parseFloat(n)) && isFinite(n) == true) {
-        money = Number(n);
-    } else {
-        alert('Введите число!');
-        getMoney();
-    }
+  let n = prompt('Ваш месячный доход?', 30000);
+  if (!isNaN(parseFloat(n)) && isFinite(n) == true) {
+    money = Number(n);
+  } else {
+    alert('Введите число!');
+    getMoney();
+  }
 }
 getMoney();
 
@@ -47,11 +47,11 @@ budgetDay = Math.floor(budgetDay);
 console.log('Доход за день с учётом расходов: ' + budgetDay + ' рублей');//доход за день
 
 let getExpensesMonth = function () {
-    return spending1 + spending2;//сумма всех расходов
+  return spending1 + spending2;//сумма всех расходов
 };
 
 let getAccumulateMonth = function () {
-    return money - getExpensesMonth();//накопления за месяц(минус расходы)
+  return money - getExpensesMonth();//накопления за месяц(минус расходы)
 };
 
 let accumulatedMonth = getAccumulateMonth();
@@ -59,32 +59,39 @@ let accumulatedMonth = getAccumulateMonth();
 console.log('Расходы за месяц: ' + getExpensesMonth());
 
 let getTargetMonth = function () {
-    period = Math.floor(mission / accumulatedMonth);//расчёт периода достижениея цели
-    return period;
+  period = Math.floor(mission / accumulatedMonth);//расчёт периода достижениея цели
+  return period;
 };
 getTargetMonth();
+
+let periodAccumulate = 0;
+let getPeriodAccumulate = function () {
+  periodAccumulate = period * accumulatedMonth;
+};
+getPeriodAccumulate();//все накопления за период
 
 console.clear();//чистка консоли
 
 let showTypeOf = function (data) {
-    console.log(data, typeof (data));
+  console.log(data, typeof (data));
 };
 showTypeOf(money);
 showTypeOf(income);
 showTypeOf(deposit);
 
 let getStatusIncome = function () {
-    if (budgetDay >= 800) {
-        return ('Высокий уровень дохода');
-    } else if (budgetDay >= 300 && budgetDay < 800) {
-        return ('Средний уровень дохода');
-    } else if (budgetDay >= 0 && budgetDay < 300) {
-        return ('Низкий уровень дохода');
-    } else {
-        return ('Что-то пошло не так');
-    }
+  if (budgetDay >= 800) {
+    return ('Высокий уровень дохода');
+  } else if (budgetDay >= 300 && budgetDay < 800) {
+    return ('Средний уровень дохода');
+  } else if (budgetDay >= 0 && budgetDay < 300) {
+    return ('Низкий уровень дохода');
+  } else {
+    return ('Что-то пошло не так');
+  }
 };
 
 console.log(getStatusIncome());
 console.log('Накопления за месяц: ' + accumulatedMonth);
+console.log('Накопления за период: ' + periodAccumulate);//накопления за период
 console.log(`Цель будет достигнута за ${period} месяца(ев)`);//вывод срок достижения цели
