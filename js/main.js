9f6f686f9792d33f2c290ac8606daa7d20f61784
Nikolay <nikolka-net@mail.ru>
2019-10-24 19:55:19 +0600

    Переписал start, отриц. значение в getTargetMonth

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@
 
 let mission = 100000,
     period = 5,
+    income = 'Ремонт пк',
     money;
 
-function getMoney() {
+/* function getMoney() {
     let n = prompt('Ваш месячный доход?', 30000);
     if (!isNaN(parseFloat(n)) && isFinite(n) == true) {
         money = Number(n);
@@ -13,58 +14,98 @@ function getMoney() {
         getMoney();
     }
 }
-getMoney();
+getMoney(); */
 
-let income = 'Ремонт пк';
+let start = function () {
+    do {
+        money = prompt('Ваш месячный доход?', 30000);
+        console.log('money: ', money);
+    }
+    while (isNaN(money) || money === '' || money === null);
+};
+start();
+
+
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'квартплата, интернет');
 addExpenses = addExpenses.split(', ');
 console.log(addExpenses);
+
 let deposit = !!confirm('Есть ли у вас депозит в банке?');
-
-
-
-
-let addExpenses1 = prompt('Какие ежемесячные расходы у вас есть?', 'телефон');
-let spending1 = +prompt('Во сколько это обойдётся?', '1000');
-
-let addExpenses2 = prompt('Какие ежемесячные расходы у вас есть?', 'питание');
-let spending2 = +prompt('Во сколько это обойдётся?', '2500');
 
 console.log('Период ' + period + ' месяца');
 console.log(`Цель заработать ${mission} рублей`);
 
-let budgetMonth = money - (spending1 + spending2);
+
+
+// let spending1 = +prompt('Во сколько это обойдётся?', '1000');
+// let spending2 = +prompt('Во сколько это обойдётся?', '2500');
+
+let addExpenses1,
+    addExpenses2;
+
+let getExpensesMonth = function () {
+    let sum = 0;
+
+
+    for (let i = 0; i < 2; i++) {
+        if (i === 0) {
+            addExpenses1 = prompt('Какие ежемесячные расходы у вас есть?', 'телефон');
+        }
+        if (i === 1) {
+            addExpenses2 = prompt('Какие ежемесячные расходы у вас есть?', 'питание');
+        }
+
+        do {
+
+            sum += +prompt('Во сколько это обойдётся?', '2500');
+            console.log('sum: ', sum);
+        }
+        while (isNaN(sum) || sum === '' || sum === null);
+
+
+    }
+
+    return sum;
+};
+let spendingAmount = getExpensesMonth();
+
+
+let budgetMonth = money - (spendingAmount);
 console.log('Доход за месяц: ' + budgetMonth);
 
 let destination = mission / budgetMonth;
-destination = Math.ceil(destination);
-console.log('Цель будет достигнута за ' + destination + ' месяца(ев)');//цель достигнута за n месяцев
-
-
+if (budgetMonth > 0) {
+    destination = Math.ceil(destination);
+    console.log('Цель будет достигнута за ' + destination + ' месяца(ев)');//цель достигнута за n месяцев
+} else {
+    console.log('Цель не будет достигнута');//вывод срок достижения цели
+}
 
 let budgetDay = budgetMonth / 30;
 budgetDay = Math.floor(budgetDay);
 console.log('Доход за день с учётом расходов: ' + budgetDay + ' рублей');//доход за день
 
-let getExpensesMonth = function () {
-    return spending1 + spending2;//сумма всех расходов
-};
 
 let getAccumulateMonth = function () {
-    return money - getExpensesMonth();//накопления за месяц(минус расходы)
+    return money - spendingAmount;//накопления за месяц(минус расходы)
 };
 
 let accumulatedMonth = getAccumulateMonth();
 
-console.log('Расходы за месяц: ' + getExpensesMonth());
+console.log('Расходы за месяц: ' + spendingAmount);
 
 let getTargetMonth = function () {
-    period = Math.floor(mission / accumulatedMonth);//расчёт периода достижениея цели
-    return period;
+    if (accumulatedMonth > 0) {
+        period = Math.floor(mission / accumulatedMonth);//расчёт периода достижения цели
+        console.log(`Цель будет достигнута за ${period} месяца(ев)`);//вывод срок достижения цели
+        return period;
+    } else {
+        console.log('Цель не будет достигнута');//вывод срок достижения цели
+    }
 };
 getTargetMonth();
 
-console.clear();//чистка консоли
+//console.clear();//чистка консоли
 
 let showTypeOf = function (data) {
     console.log(data, typeof (data));
@@ -87,4 +128,3 @@ let getStatusIncome = function () {
 
 console.log(getStatusIncome());
 console.log('Накопления за месяц: ' + accumulatedMonth);
-console.log(`Цель будет достигнута за ${period} месяца(ев)`);//вывод срок достижения цели
