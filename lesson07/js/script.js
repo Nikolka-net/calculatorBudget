39e022969bb9ab9dd1a78d53eb89ac805e89ac63
Nikolay <nikolka-net@mail.ru>
2019-11-02 20:49:48 +0600

    Обновил названия некоторых переменных

--- lesson07/js/script.js
+++ lesson07/js/script.js
@@ -3,52 +3,44 @@
 
 //кнопка рассчитать
 let buttonStart = document.getElementById('start');
-console.log(buttonStart);
 
 //2 кнопки плюс
-let buttonIncome = document.getElementsByTagName('button')[0];
-let buttonExpenses = document.getElementsByTagName('button')[1];
-console.log(buttonIncome);
-console.log(buttonExpenses);
+let buttonIncomePlus = document.getElementsByTagName('button')[0];
+let buttonExpensesPlus = document.getElementsByTagName('button')[1];
 
 //чекбокс депозит
-let checkboxDeposit = document.querySelector('#deposit-check');
-console.log(checkboxDeposit);
+let depositCheck = document.querySelector('#deposit-check');
 
 //возможные доходы
-let inputAdditionalIncomeItem = document.querySelectorAll('.additional_income-item');
-console.log(inputAdditionalIncomeItem);
+let additionalIncomeItem = document.querySelectorAll('.additional_income-item');
 
 //блоки result
 let resultValue = document.querySelectorAll('.result-total');
-/* let resultBudgetMonth = resultValue[0];
-let resultBudgetDay = resultValue[1];
-let resultExpensesMonth = resultValue[2];
-let resultAdditionalIncome = resultValue[3];
-let resultAdditionalExpenses = resultValue[4];
-let resultIncomePeriod = resultValue[5];
-let resultTargetMonth = resultValue[6]; */
+let budgetMonthValue = resultValue[0];
+let budgetDayValue = resultValue[1];
+let expensesMonthValue = resultValue[2];
+let additionalIncomeValue = resultValue[3];
+let additionalExpensesValue = resultValue[4];
+let incomePeriodValue = resultValue[5];
+let targetMonthValue = resultValue[6];
 
-console.log(resultValue);
 
 //инпуты, оставшиеся поля
 
-let expenses = document.querySelectorAll('.expenses-title');
-let inputExpensesTitle = expenses[1];
-let inputExpensesAmount = document.querySelector('.expenses-amount');
-console.log(inputExpensesTitle, inputExpensesAmount);
+let salaryAmount = document.querySelector('.salary-amount');
+let incomeTitle = document.querySelector('.income-title');
+let incomeAmount = document.querySelector('.income-amount');
 
-let inputAdditionalExpensesItem = document.querySelector('.additional_expenses-item');
-console.log(inputAdditionalExpensesItem);
+let expenses = document.querySelectorAll('.expenses-title');
+let expensesTitle = expenses[1];
+let expensesAmount = document.querySelector('.expenses-amount');
+
+let additionalExpenses = document.querySelector('.additional_expenses-item');
 
 let inputDepositAmount = document.querySelector('.deposit-amount');
 let inputDepositPercent = document.querySelector('.deposit-percent');
-console.log(inputDepositAmount, inputDepositPercent);
 
 let inputTargetAmount = document.querySelector('.target-amount');
-console.log(inputTargetAmount);
 
 let inputPeriodSelect = document.querySelector('.period-select');
 let inputPeriodSelectType = inputPeriodSelect.getAttribute('type');
-console.log(inputPeriodSelect);
-console.log(inputPeriodSelectType);
